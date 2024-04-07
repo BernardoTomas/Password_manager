e4d71323288e59721c302e960594bbc2f6efc95b
Bernardo Tomas <bernardotomas.c14@gmail.com>
2024-04-07 18:31:38 -0300

    Fiz o requisito 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [formVisibility, setFormVisibility] = useState(false);
+
   return (
     <div>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      {formVisibility ? (
+        <Form />
+      ) : (
+        <button
+          onClick={() => setFormVisibility(!formVisibility)}
+        >
+          Cadastrar nova senha
+        </button>
+      )}
     </div>
   );
 }
